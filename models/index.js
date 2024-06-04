56b5d5a81fefd64a994cd2b995976ff40008fb47
anullator <danyelle.joy@gmail.com>
2024-06-04 18:17:08 -0500

    created all routes

--- models/index.js
+++ models/index.js
@@ -5,19 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE'
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE',
+    through: ProductTag
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+    foreignKey: 'tag_id',
+    onDelete: 'CASCADE',
+    through: ProductTag
 });
 
 module.exports = {
